1712e39e6dd539774cae353c16b01c875f4bdf4c
whalay <soliuy5@gmail.com>
2023-06-05 15:56:56 +0100

    added DarkMode functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,12 @@
-import { useState, useEffect } from "react";
-import { Routes, Route, Outlet } from "react-router";
-import { DarkModeContext, DarkModeProvider } from "./context/DarkModeContext";
-import Dropdown from "./components/Dropdown";
+
+import { Routes, Route} from "react-router";
+import { DarkModeProvider } from "./context/DarkModeContext";
+
 import Header from "./components/Header";
 import CountryDetail from "./pages/CountryDetail";
 import Homepage from "./pages/Homepage";
 
 function App() {
-  // const [data, setData] = useState([]);
-  // const [error, setError] = useState(null);
-  // const [isLoad, setIsLoad] = useState(true);
-
-  // useEffect(() => {
-  //   const getData = async () => {
-  //     // const response = await fetch(`https://restcountries.com/v3.1/all`);
-  //     // const actualData = await response.json();
-
-  //     // setData(actualData);
-  //     // console.log(actualData);
-
-  //     // setIsLoad(false);
-
-  //       const response = await fetch(`https://restcountries.com/v3.1/all`);
-  //       if (!response.ok) {
-  //         throw new Error(
-  //           `This is an HTTP error: This status is ${response.status}`
-  //         );
-  //       }
-  //       let actualData = await response.json();
-  //       console.log(actualData);
-  //       setData(actualData);
-  //       console.log(data);
-  //       setIsLoad(false);
-  //     // } catch (err) {
-  //     //   setError(err.message);
-  //     // }
-  //   };
-  //   getData();
-  // }, []);
-
-  // const getData = () => {
-  //   fetch("https://restcountries.com/v3.1/all")
-  //     .then((res) => res.json())
-  //     .then((results) => {
-
-  //       setData(results);
-  //       console.log(data);
-  //       console.log(results);
-  //       setIsLoad(false);
-
-  //     });
-  //     console.log(data);
-  // };
-
   return (
     <DarkModeProvider>
       <Header />
--- src/pages/js.js
+++
@@ -1,8 +0,0 @@
-// let count = 0
-// console.log(++count)
-// console.log(count)
-
-
-let count = 0
-console.log(count++)        
-console.log(count)          
